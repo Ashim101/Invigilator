7b7b87a2691c97dd02e257bfb6648b5e1c807448
Ashim101 <ashimsapkota250@gmail.com>
2023-07-04 01:27:12 +0545

    first

---
+++ home/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    pass
+
+class exam(models.Model):
+    pass
+class room(models.Model):
+    pass
+class building(models.Model):
+    pass
+class invigilator(models.Model):
+    pass
---
+++ home/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def register(request):
+    if(request.method=="POST"):
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        username=request.POST.get("username")
+        exist = User.objects.filter(username=username)
+        if exist.exists():
+            messages.error(request, "username already exists")
+            return redirect("/register/")
+        user=User.objects.create(first_name=first_name,last_name=last_name,username=username) 
+        user.set_password(request.POST.get("password1"))
+        user.save()
+        messages.info(request, "Account created successfully")
+
+        return redirect("/register/")
+    return render(request,"signup.html")
+def login_page(request):
+    if(request.method=="POST"):
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        print(username+password)
+        user=authenticate(username=username,password=password)
+        if user is None:
+            messages.error(request,"User doesnot exists")
+        else:
+            login(request,user)
+            return redirect('/register')
+    return render(request,"signin.html")
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('/login')
